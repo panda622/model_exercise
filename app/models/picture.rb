6d4d72ee7975b98a7d7a0e7522651ad3f1ba472d
Bao Dang <baodq@atmarkcafe.org>
2018-08-09 14:24:28 +0700

    update

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,21 +2,9 @@ require "open-uri"
 
 class Picture < ApplicationRecord
   belongs_to :article, optional: true
-  before_create :generate_position
-
+  default_scope { order('position') }
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-  def generate_position
-    p1 = Text.last.try(:position)
-    p2 =  Picture.last.try(:position)
-
-    p1 ||= 0
-    p2 ||= 0
-
-    p = p1 > p2 ? p1 : p2
-    self.position = p + 1
-  end
 
    def picture_from_url(url)
     self.image = open(url)
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,20 +1,8 @@
 class Text < ApplicationRecord
   belongs_to :article, optional: true
-
-  before_create :generate_position
-
-  def generate_position
-    p1 = Text.last.try(:position)
-    p2 =  Picture.last.try(:position)
-
-    p1 ||= 0
-    p2 ||= 0
-
-    p = p1 > p2 ? p1 : p2
-    self.position = p + 1
-  end
+  default_scope { order('position ASC') }
 
   scope :get_sentences,  -> (article_id) { where('article_id = ?', article_id) }
- 
+
 
 end
